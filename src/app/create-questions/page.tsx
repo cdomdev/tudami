cc7fd1d05e24dcac6b78e4c0142a4a7c780c89d8
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-08-06 09:38:00 +0000

    ajuate de la posicion del titulo en la creacion de pregunta

--- src/app/create-questions/page.tsx
+++ src/app/create-questions/page.tsx
@@ -10,11 +10,11 @@ export const metadata: Metadata = {
 export default function QuestionsPage() {
   return (
     <>
-      <h1 className="relative -left-1/4 mb-2 mt-30 text-center font-medium  text-xl md:text-2xl text-primary">
+      <h1 className="relative md:-left-1/4 mb-2 mt-24 md:mt-30 text-center font-medium  text-xl md:text-2xl text-primary">
         ¿En qué necesitas ayuda?
       </h1>
-      <section className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-6 gap-4">
-        <article className="md:col-span-4 w-full shadow-sm order-2 lg:order-1 dark:bg-custom-card rounded-sm">
+      <section className="max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-6 gap-4 px-2">
+        <article className="md:col-span-4 px w-full shadow-sm order-2 lg:order-1 dark:bg-custom-card rounded-sm">
           <Editor />
         </article>
         <article className="md:col-span-2 p-6 shadow-sm dark:bg-custom-card rounded-sm order-1 lg:order-2">
