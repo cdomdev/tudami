2f83bf94b2cfea0039e126e43a9ac724640208ff
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-08-11 09:17:33 +0000

    feat: agreo rutas para la creacion y solictud de ofertas asjute de funciones para los componentes de ofertas

---
+++ src/schemas/schema.offers.ts
@@ -0,0 +1,24 @@
+import z from "zod";
+import { User } from '@/schemas/schema.user'
+
+export const SchemaResOffers = z.object({
+    id: z.number(),
+    user_id: z.string(),
+    title: z.string(),
+    details: z.string(),
+    status: z.string(),
+    users: User,
+    offers_aplication: z.number(),
+    created_at: z.coerce.date(),
+});
+
+export type SchemaOffers = z.infer<typeof SchemaResOffers>;
+
+
+export const Response = z.object({
+    data: z.array(SchemaResOffers),
+    count: z.number(),
+});
+
+
+export type SchemaOffersResponse = z.infer<typeof Response>;
