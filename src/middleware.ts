f2a645d97045309098421532d1d5f245e0d90428
dominguez5493.cd <dominguez5493.cd@gmail.com>
2025-09-01 13:35:35 +0000

    remove logs of middelware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,24 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "@/utils/supabase/supabaseClient"
 
 export async function middleware(req: NextRequest) {
   const accessToken = req.cookies.get("sb-access-token")?.value;
   const refreshToken = req.cookies.get("sb-refresh-token")?.value;
 
-  // Si no hay token de acceso, redirigir a login
   if (!accessToken) {
     return redirectToLogin(req);
   }
 
   try {
-    // Intentar verificar el token de acceso
-    const supabase = createClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-    );
 
     const { data: userSession, error: sessionError } = await supabase.auth.getUser(accessToken);
 
-    // Si el token es válido, permitir la solicitud
     if (userSession?.user && !sessionError) {
       return NextResponse.next();
     }
 
-    // Si el token no es válido pero hay un token de refresco, intentar renovar la sesión
     if (refreshToken) {
-      // En lugar de renovar aquí, redirigimos a una ruta especial
       const url = req.nextUrl.clone();
       const originalPath = req.nextUrl.pathname;
 
@@ -37,7 +28,6 @@ export async function middleware(req: NextRequest) {
       return NextResponse.redirect(url);
     }
 
-    // Si no hay token de refresco o hay algún otro problema, redirigir a login
     return redirectToLogin(req);
   } catch (error) {
     console.error("Error en middleware:", error);
