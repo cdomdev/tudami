797e8cac8a8ededd16f3560ba31f37a8b9111b79
cdomdev <dominguez5493.cd@gmail.com>
2025-09-30 18:18:58 -0500

    update  next-sitemap.config.ts

--- next-sitemap.config.ts
+++ next-sitemap.config.ts
@@ -1,15 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://miapp.vercel.app',
-    generateRobotsTxt: true,
-    exclude: ['/api/*'],
-    robotsTxtOptions: {
-        policies: [
-            {
-                userAgent: '*',
-                allow: '/',
-                disallow: ['/api'],
-            },
-        ],
-    },
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || "https://tudami.com/",
+  generateRobotsTxt: true,
+  exclude: ["/api/*"],
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/api"],
+      },
+    ],
+  },
 };
