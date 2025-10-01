76583382f72bae85a5d06839b310faf59d17da1e
cdomdev <dominguez5493.cd@gmail.com>
2025-09-30 19:36:59 -0500

    fix route

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,23 +1,23 @@
-import { PasswordUpdated } from "@/emails/PasswordUpdated";
-import { Resend } from "resend";
+// import { PasswordUpdated } from "@/emails/PasswordUpdated";
+// import { Resend } from "resend";
 
-const resend = new Resend(process.env.NEXT_PUBLIC_API_KEY_RESEND);
+// const resend = new Resend(process.env.NEXT_PUBLIC_API_KEY_RESEND);
 
-export async function POST() {
-  try {
-    const { data, error } = await resend.emails.send({
-      from: "Equipo tudami <team@info.tudami.com>",
-      to: ["cdomreyes@gmail.com"],
-      subject: "Hello world",
-      react: PasswordUpdated({ userName: "carlos" }),
-    });
+// export async function POST() {
+//   try {
+//     const { data, error } = await resend.emails.send({
+//       from: "Equipo tudami <team@info.tudami.com>",
+//       to: ["cdomreyes@gmail.com"],
+//       subject: "Hello world",
+//       react: PasswordUpdated({ userName: "carlos" }),
+//     });
 
-    if (error) {
-      return Response.json({ error }, { status: 500 });
-    }
+//     if (error) {
+//       return Response.json({ error }, { status: 500 });
+//     }
 
-    return Response.json(data);
-  } catch (error) {
-    return Response.json({ error }, { status: 500 });
-  }
-}
+//     return Response.json(data);
+//   } catch (error) {
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
