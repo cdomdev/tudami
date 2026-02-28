b1bda9197ebda42deb1ebaeabe43b1433aface10
cdomdev <dominguez5493.cd@gmail.com>
2026-02-27 20:57:18 -0500

    fix: corregir estilos de borde y ajustar ancho mínimo en la página de autenticación

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -26,12 +26,12 @@ export default function AuthPage() {
         </p>
       </div>
 
-      <div className="flex flex-col gap-3 min-w-[300px] w-full">
+      <div className="flex flex-col gap-3 min-w-75 w-full">
         <ProvidersAuth />
         <div className="w-full flex items-center gap-1">
-          <div className="border-1 w-full border-gray-300"></div>
+          <div className="border w-full border-gray-300"></div>
           <span className="font-bold">o</span>
-          <div className="border-1 w-full border-gray-300"></div>
+          <div className="border w-full border-gray-300"></div>
         </div>
         <FormLogin />
         <FooterForm />
